732eeacf93db4c3274a7886c335a1c396b3fff99
Kerwin <kerwin@limabean.co.za>
2019-12-10 16:50:20 +0200

    splicetest - add basket functionality - basic

--- js/app.js
+++ js/app.js
@@ -36,6 +36,100 @@ const navbarFunc = () => {
   handleScroll();
 };
 
+const handleBasket = () => {
+  let products = [
+    { id: "berry", price: 36.0, qty: 0 },
+    { id: "sundried", price: 72.0, qty: 0 },
+    { id: "veg", price: 22.0, qty: 0 },
+    { id: "man", price: 292.0, qty: 0 }
+  ];
+
+  let total = 0;
+
+  const init = () => {
+    document
+      .querySelectorAll(".add-to-cart")
+      .forEach(node => node.addEventListener("click", addToCart));
+
+    document
+      .querySelectorAll(".delete-product")
+      .forEach(node => node.addEventListener("click", removeFromCart));
+  };
+
+  const addToCart = e => {
+    const value = e.target.value;
+    products.forEach(product => {
+      if (product.id === value) {
+        if(product.qty === 0){
+          showProduct(value)
+        }
+        product.qty++;
+        udpateQty(value, product.qty);
+      }
+    });
+
+    updateTotal(value)
+    updateCountDisplay(countProducts(products))
+  };
+
+  const removeFromCart = e => {
+    const value = e.target.dataset.value;
+    products.forEach(product => {
+      if (product.id === value) {
+        product.qty--;
+        udpateQty(value, product.qty);
+        if(product.qty < 1){
+          hideProduct(value)
+        }
+      }
+    });
+    updateTotal(value, '-')
+    updateCountDisplay(countProducts(products))
+  };
+
+  const countProducts = (prodArr) => {
+    let count = 0;
+    prodArr.forEach(prod => count += prod.qty)
+    return count;
+  }
+
+  const udpateQty = (prodId, value) => {
+    document.querySelector(`#${prodId}-product .qty`).innerHTML = `${value}`
+  }
+
+  const hideProduct = (prodKey) => {
+    const product = document.getElementById(`${prodKey}-product`)
+    if(product){
+      product.classList.add('hidden')
+    }
+  }
+
+  const showProduct = (prodKey) => {
+    const product = document.getElementById(`${prodKey}-product`);
+    if(product){
+      product.classList.remove('hidden')
+    }
+  }
+
+  const updateTotal = (value, direction = '+') => {
+    const price = products.filter(product => product.id === value)[0].price
+    direction === '+' ? total += price : total -= price;
+
+    updateTotalDisplay(total);
+  }
+
+  const updateTotalDisplay = (total) => {
+    document.querySelector('.total-amount').innerHTML = `${total}`;
+  }
+
+  const updateCountDisplay = (count) => {
+    document.querySelectorAll('.total-count').forEach(node => node.innerHTML = `${count}`);
+  }
+
+  init();
+};
+
 $(document).ready(function() {
   navbarFunc();
+  handleBasket();
 });
